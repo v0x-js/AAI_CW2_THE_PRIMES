42f31f69a53cce2864470b9bc773ada32a41c382
KalebKaneS2339097 <kkane301@caledonian.ac.uk>
2026-04-14 15:18:56 +0100

    Update Main_Script_Arduino.ino

--- Main_Script_Arduino.ino
+++ Main_Script_Arduino.ino
@@ -16,9 +16,19 @@ Serial.begin(9600); // Setting the serial baud rate
 
 
 
-//send number depending on NFC tag reading and the piezo sensor value exceeding the threshold 
+//send number to MAX depending on NFC tag reading if the piezo sensor value exceeds the threshold 
 
-if(val >= PIEZOTHRESHOLD) { 
-  
+if ((val >= PIEZOTHRESHOLD) && RFID_Value1) { // These are the conditions for number being sent to MAX
+  Serial.println(1);        // Sends the number 1 to select the correct voice modulator in MAX 9
+
 }
+if ((val >= PIEZOTHRESHOLD) && RFID_Value2) { // These are the conditions for number being sent to MAX
+  Serial.println(2);        // Sends the number 2 to select the correct voice modulator in MAX 9
+
+}
+if ((val >= PIEZOTHRESHOLD) && RFID_Value3) { // These are the conditions for number being sent to MAX
+  Serial.println(3);        // Sends the number 3 to select the correct voice modulator in MAX 9
+
+}
+
 }
