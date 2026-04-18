6471471e59271c95d978296943c5860663167e30
KalebKaneS2339097 <kkane301@caledonian.ac.uk>
2026-04-18 16:56:42 +0100

    Piezo Logic
    KK-18/04/26- Changes to Logic for Piezo

--- Main_Script_Arduino/piezoComTest/piezoComTest.ino
+++ Main_Script_Arduino/piezoComTest/piezoComTest.ino
@@ -1,14 +1,14 @@
 #define PIEZOTHRESHOLD 5;  // this is the analogue threshold for piezo sensing (subject to change on testing likley will be higher number)  
-#define PADNUM 1 //1 pad in the analogue pins
+#define PADNUM 1; //1 pad in the analogue pins
 
 // setting default values for watch press + creating variables for watch press
 int val; // used to store value of current piezo sensor input to check if threshold for piezo sesnor is being exceeded or not
 bool Pressed = false; //variable to say if piezo is being pushed or released
 bool piezoPrevState = false;  //allows for comparison to previous state
 bool piezoStateChange = false; // has there been a change in the state - true or false
-bool piezoRelease = false; 
 
-bool StopData = false; 
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,7 +23,6 @@ void loop() {
     Pressed = true;
     if (Pressed != piezoPrevState) { //if the comparison is different from the current bool value a state change has occured
       piezoStateChange = true;
-      piezoRelease = false; 
     }
     piezoPrevState = Pressed; //sets the prev pressed state value to the current pressed state value for comparison on the next time the main void loop loops
   }
@@ -32,32 +31,16 @@ void loop() {
     Pressed = false;
     if (Pressed != piezoPrevState) {
       piezoStateChange = true;
-      piezoRelease = true; 
     }
      piezoPrevState = Pressed;
   }
 
-  if ((Pressed = true) && (piezoStateChange = true) && (piezoRelease = false) && (StopData = false)) {
-    
+  if ((Pressed = true) && (piezoStateChange = true)) {
     Serial.println(1);
-    StopData = true; 
   }
 
-  if (StopData = true) {
-    piezoStateChange = false; 
-  } 
-
-
-  if ((Pressed = false) && (piezoStateChange = true) && (piezoRelease = true) && (StopData = true)) {
-  
+  if ((Pressed = false) && (piezoStateChange = false)) {
     Serial.println(4); 
-    StopData = false; 
   }
-  
-  if (StopData = false) {
-    piezoStateChange = false;
-  }
-
-
 }
 
