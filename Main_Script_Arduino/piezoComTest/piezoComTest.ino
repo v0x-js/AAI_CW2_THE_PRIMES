27c31114e9581dcfe9783552728a87c43b843acf
KalebKaneS2339097 <kkane301@caledonian.ac.uk>
2026-04-18 17:28:01 +0100

    Finale
    KK - 18/04/26 - Update on code for Piezo

--- Main_Script_Arduino/piezoComTest/piezoComTest.ino
+++ Main_Script_Arduino/piezoComTest/piezoComTest.ino
@@ -6,9 +6,6 @@ int val; // used to store value of current piezo sensor input to check if thresh
 bool Pressed = false; //variable to say if piezo is being pushed or released
 bool piezoPrevState = false;  //allows for comparison to previous state
 bool piezoStateChange = false; // has there been a change in the state - true or false
-bool piezoRelease = false; 
-
-bool StopData = false; 
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,49 +18,20 @@ void loop() {
 
   if ( val > 5 ) { //checks if the current value is above the threshold
     Pressed = true;
-    if (Pressed != piezoPrevState) { //if the comparison is different from the current bool value a state change has occured
-      piezoStateChange = true;
-      piezoRelease = false; 
+    piezoStateChange = true;
     }
-  //  piezoPrevState = Pressed; //sets the prev pressed state value to the current pressed state value for comparison on the next time the main void loop loops
-  }
-    //essiential the same as code above but in reverse for when the dial is released from the closed position
+
+
   if ( val < 5 ) {
     Pressed = false;
-    if (Pressed == piezoPrevState) {
-      piezoStateChange = true;
-      piezoRelease = true; 
-    }
+    piezoStateChange = true;
   }
-<<<<<<< Updated upstream
+  
 
-  if ((Pressed = true) && (piezoStateChange = true) && (piezoRelease = false) && (StopData = false)) {
+  if ((Pressed = true) && (piezoStateChange = true) ) {
     
     Serial.println(1);
-    StopData = true; 
+    delay(300); 
   }
-
-  if (StopData = true) {
-    piezoStateChange = false; 
-  } 
-
-
-  if ((Pressed = false) && (piezoStateChange = true) && (piezoRelease = true) && (StopData = true)) {
-  
-    Serial.println(4); 
-    StopData = false; 
-  }
-  
-  if (StopData = false) {
-    piezoStateChange = false;
-  }
-
-
-=======
-  if ((Pressed == true) && (piezoStateChange == true)) {
-    Serial.println(1);
-    delay(300);
-  } 
->>>>>>> Stashed changes
 }
 
